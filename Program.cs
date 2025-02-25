fdd87f2b2479da1391e844f518b5bb735ad2a8b5
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-02-25 11:37:07 +0000

    Added reading CSV file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.IO;
 
 namespace SupportBank {
     class Program {
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string filePath = "./data/Transactions2014.csv";
+            List<string> transactions = [];
+            CSVReader.ReadFIle(filePath, transactions);
+            Printer.PrintList(transactions);
+        }
+    }
+
+    class CSVReader {
+        public static void ReadFIle(string filePath, List<string> transactions) {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    transactions.Add(line);
+                }
+            }
+        }
+    }
+
+    class Printer {
+        public static void PrintList(List<string> list) {
+            list.ForEach(listItem => Console.WriteLine(listItem));
         }
     }
 }
