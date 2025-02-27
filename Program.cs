0f168464462b4bf9f5f7d316009526fb527fb8ca
Preeti Mishra <preeti.mishra@softwire.com>
2025-02-27 16:26:41 +0000

    Logging and exception handling.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
-﻿namespace SupportBank
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+namespace SupportBank
 {
     class Program
     {
         public static void Main(string[] args)
         {
             string filePath = "./data/Transactions2014.csv";
-            List<string> fileLines = CSVReader.ReadFile(filePath);
-            List<Transaction> transactions = CSVReader.CreateTransactionList(fileLines);
-            SupportBank supportBank = new();
-            supportBank.CreatePersonList(transactions);
-            ConsoleApp.Run(supportBank.personList);
+            // string filePath = "./data/DodgyTransactions2015.csv";
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+            try
+            {
+                if (CSVReader.ValidateCSVFile(filePath))
+                {
+                    List<string> fileLines = CSVReader.ReadFile(filePath);
+                    List<Transaction> transactions = CSVReader.CreateTransactionList(fileLines);
+                    SupportBank supportBank = new();
+                    supportBank.CreatePersonList(transactions);
+                    ConsoleApp.Run(supportBank.personList);
+                }
+                else
+                {
+                    throw new FormatException("CSV file is not in a proper format.");
+                }
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+
         }
     }
 }
--- src/utils/CSVReader.cs
+++ src/utils/CSVReader.cs
@@ -1,40 +1,70 @@
+using System.Runtime.InteropServices;
+using System.Globalization;
+using NLog;
+
 namespace SupportBank
 {
     class CSVReader
     {
 
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public static bool ValidateCSVFile(string filePath)
+        {
+
+            List<string> fileLines = ReadFile(filePath);
+            bool isValid = true;
+
+            for (int line = 1; line < fileLines.Count; line++)
+            {
+                string[] lineParts = fileLines[line].Split(",");
+                string logMessage = $" was not recognized as a valid value in the file {filePath} at line number {line + 1}";
+                if (!DateTime.TryParse(lineParts[0], out DateTime parsedDate))
+                {
+                    Logger.Error($" '{lineParts[0]}' {logMessage}");
+                    isValid = false;
+
+                }
+                if (!decimal.TryParse(lineParts[4], NumberStyles.Currency, CultureInfo.InvariantCulture, out decimal amount))
+                {
+                    Logger.Error($" '{lineParts[4]}' {logMessage}");
+                    isValid = false;
+                }
+            }
+            return isValid;
+
+        }
         public static List<string> ReadFile(string filePath)
         {
-            
             using StreamReader reader = new(filePath);
             string? line;
             List<string> lines = [];
-            
+
             while ((line = reader.ReadLine()) != null)
             {
                 lines.Add(line);
             }
             return lines;
-            
+
         }
         public static List<Transaction> CreateTransactionList(List<string> fileLines)
         {
             List<Transaction> transactions = [];
-            for (var i = 1; i < fileLines.Count; i++) {
+
+            for (var i = 1; i < fileLines.Count; i++)
+            {
                 string[] transactionParts = fileLines[i].Split(",");
-                
+
                 Transaction transaction = new Transaction(
                     Convert.ToDateTime(transactionParts[0]),
                     transactionParts[1],
                     transactionParts[2],
                     transactionParts[3],
-                    AmountConversion.ConvertPoundToPence(transactionParts[4])                 
+                    AmountConversion.ConvertPoundToPence((transactionParts[4]))
                 );
                 transactions.Add(transaction);
-
             }
             return transactions;
         }
-       
     }
 }
